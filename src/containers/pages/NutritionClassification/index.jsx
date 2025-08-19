7d989bede9f15c0df81001963c7841cec90ed2a9
Rangga Wiraguna <ranggawiragunar@gmail.com>
2025-08-19 10:56:39 +0700

    Add Initial Page for Nutrition Classification

--- src/containers/pages/NutritionClassification/index.jsx
+++ src/containers/pages/NutritionClassification/index.jsx
@@ -3,7 +3,13 @@ import { useEffect } from 'react';
 import { MENU_OPEN } from 'utils/redux/action';
 import { useDispatch, useSelector } from 'react-redux';
 import PageRoot from './styled';
+import SaveIcon from '@mui/icons-material/Save';
 import PageContentHeader from 'components/elements/PageContentHeader';
+import { Box, Button, Grid, Typography } from '@mui/material';
+import BootstrapInput from 'components/elements/BootstrapInput/styled';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { IconUserPlus } from '@tabler/icons';
 
 export default function NutritionClassificationPage() {
   const dispatch = useDispatch();
@@ -22,7 +28,97 @@ export default function NutritionClassificationPage() {
 
   return (
 	<PageRoot>
-		<PageContentHeader title="Klasifikasi Gizi" />
+		<PageContentHeader title="Klasifikasi Gizi" buttonIconHidden buttonText="Periksa Hasil" buttonAction={()=>{}} />
+		<Grid container spacing={2}>
+			<Grid item xs={12} sm={6}>
+				<Typography variant="h4" gutterBottom>
+					Nama Pemeriksa
+				</Typography>
+				<BootstrapInput />
+			</Grid>
+			<Grid item xs={12} sm={6} sx={{ position: 'relative' }}>
+				<Typography variant="h4" gutterBottom>
+					Tanggal periksa
+				</Typography>
+				<LocalizationProvider dateAdapter={AdapterDayjs}>
+					<DatePicker
+						label="Tanggal Lahir"
+						value={null}
+						onChange={(_) => null}
+						renderInput={({ inputRef, inputProps, InputProps }) => (
+							<>
+								<BootstrapInput ref={inputRef} {...inputProps} value={""} onChange={(_)=>null} />
+								<Box sx={{ 
+									position: "absolute", 
+									marginTop:2.5,
+									top: "50%",
+									transform: "translateY(-50%)",
+									right:15, 
+									width:28, 
+									height:28, 
+									display:"flex",
+									justifyContent: "center",
+									alignItems: "center",
+								}}>
+									{InputProps?.endAdornment}
+								</Box>
+							</>
+						)}
+					/>
+				</LocalizationProvider>
+			</Grid>
+			<Grid item xs={12}>
+				<Typography variant="h4" gutterBottom>
+					Informasi Balita
+				</Typography>
+				<Box sx={{
+					width: '100%', 
+					padding: 2,
+					backgroundColor:"rgba(255,255,255,0.5)",
+					border : "3px solid rgba(255,255,255,0.5)", 
+					borderRadius: 2,
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
+					gap: 2,
+					cursor: "pointer"
+				}}>
+					<IconUserPlus />
+					<Typography variant='p' marginTop={0.5}>
+						Silahkan pilih balita yang akan diperiksa dari daftar balita yang telah terdaftar.
+					</Typography>
+				</Box>
+			</Grid>
+			<Grid item xs={12} sm={6}>
+				<Typography variant="h4" gutterBottom>
+					Tinggi Badan
+				</Typography>
+				<BootstrapInput />
+			</Grid>
+			<Grid item xs={12} sm={6}>
+				<Typography variant="h4" gutterBottom>
+					Berat Badan
+				</Typography>
+				<BootstrapInput />
+			</Grid>
+			<Grid item xs={12} >
+				<Typography variant="h4" gutterBottom>
+					Hasil Pemeriksaan
+				</Typography>
+				<BootstrapInput />
+			</Grid>
+			<Grid item xs={12} >
+				<Typography variant="h4" gutterBottom>
+					Catatan Pemeriksaan
+				</Typography>
+				<BootstrapInput />
+			</Grid>
+			<Grid item xs={12} sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+				<Button variant="contained" onClick={()=>{}} startIcon={<SaveIcon />}>
+					Simpan Hasil
+				</Button>
+			</Grid>
+		</Grid>
 	</PageRoot>
   );
 }
